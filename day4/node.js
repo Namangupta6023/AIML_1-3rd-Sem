4ec11cedf9996c27ae71f7ba4bf61a5bcc25f0b5
Naman Gupta <naman6023@gmail.com>
2026-08-19 14:24:33 +0530

    day4

---
+++ day4/node.js
@@ -0,0 +1,53 @@
+let name = "Rahul";
+const age = 20;
+var city = "Delhi";
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+let username = "Alex";
+
+function showName() {
+    console.log(username);
+}
+
+showName();
+
+let firstName = "Prateek";
+let lastName = "Kumar";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+
+// split() 
+
+let data = 'CSS , HTML , Node' ;
+let result = data.split(',') ;
+
+console.log(result) ;
+
+// include()
+
+let course = [{'hello': 'world'}, {'java': 'script'}];
+
+let date = new Date();
+
+console.log(date.getMonth());
+
+
+console.log(Math.PI);
+
+console.log(Math.max(1,2));
+
+console.log(Math.min(1,2));
+
+console.log(Math.ceil(12.34));
+
+console.log(Math.floor(123.56));
+
+
